4c2fdd75805a6b788e1cd0a6a9c45677b807960c
Puto da informática <gabrielferreira1binformatica@gmail.com>
2025-06-02 01:52:01 -0300

    apagando oque e inutil

--- site/js/carrinho.js
+++
@@ -1,7 +0,0 @@
-// Função para adicionar ao carrinho
-function adicionarAoCarrinho(produto) {
-  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-  carrinho.push(produto);
-  localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  alert("Produto adicionado ao carrinho!");
-}
